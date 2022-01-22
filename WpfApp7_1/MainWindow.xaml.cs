398d8375bf0f8d3c787115d17d201b8ae0811151
gavrilenkovi@bk.ru <gavrilenkovi@bk.ru>
2022-01-22 17:17:27 +0300

    1

---
+++ WpfApp7_1/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_2(object sender, SelectionChangedEventArgs e)
+        {
+            string saizeName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontSize = Convert.ToDouble(saizeName);
+            }
+
+        }
+        bool Bold;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Bold == false)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+            Bold = !Bold;
+        }
+        bool Italic;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Italic == false)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+            Italic = !Italic;
+        }
+        bool Underline;
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (Underline == false)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = TextDecorations.Baseline;
+            }
+            Underline = !Underline;
+
+
+
+        }
+        bool check;
+        private void RadioButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+
+                textBox.Foreground = Brushes.Black;
+
+
+        }
+        bool check1;
+        private void RadioButton2_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.Foreground = Brushes.Red;
+        }
+
+     
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+    }
+}
